2b46ccd5174148cc09b16179d271161d25f48e50
Jaime Lopez <loama18@gmail.com>
2019-11-11 15:54:43 -0600

    add navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <img class="logo" src="../assets/logo.png">
+
+      <div id="view-selector">
+        <div class="option">New</div>
+        <div class="option">Top</div>
+        <div class="option">Best</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+  },
+  mounted () {}
 }
 </script>
+
+<style lang="sass">
+  @import "@/variables.sass"
+
+  #nav
+    background: $orange
+    height: 56px
+    left: 0
+    position: fixed
+    top: 0
+    width: 100vw
+
+    .logo
+      height: 40px
+      margin: 8px
+</style>
