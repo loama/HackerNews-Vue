60a5391321c35949c05f27d87cc8c9cb1367608f
Jaime Lopez <loama18@gmail.com>
2019-11-11 16:37:01 -0600

    add example story

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,6 +24,15 @@
              v-bind:class="activeView">{{activeView}}</div>
       </div>
     </div>
+
+    <div id="filters">
+    </div>
+
+    <ul class="stories">
+      <li v-for="story in stories" v-bind:key="story.title">
+        {{story}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -32,7 +41,62 @@
 export default {
   data: function () {
     return {
-      activeView: 'new'
+      activeView: 'new',
+      stories: [
+        {
+          'by': 'aminecodes',
+          'descendants': 291,
+          'id': 21137553,
+          'kids': [
+            21138653,
+            21138113,
+            21138829,
+            21138131,
+            21142749,
+            21138320,
+            21137931,
+            21139047,
+            21139160,
+            21137857,
+            21142759,
+            21139206,
+            21137889,
+            21138187,
+            21139948,
+            21137852,
+            21139110,
+            21139717,
+            21139555,
+            21138014,
+            21137814,
+            21139211,
+            21137798,
+            21139509,
+            21142998,
+            21146914,
+            21138539,
+            21140364,
+            21139839,
+            21138185,
+            21139098,
+            21138576,
+            21138580,
+            21138914,
+            21140040,
+            21139430,
+            21137996,
+            21138618,
+            21137958,
+            21137784,
+            21139786
+          ],
+          'score': 372,
+          'time': 1570030411,
+          'title': 'Microsoft introduces Windows 10 X for dual-screen devices',
+          'type': 'story',
+          'url': 'https://techcrunch.com/2019/10/02/microsoft-introduces-windows-10-x-for-dual-screen-devices/'
+        }
+      ]
     }
   },
   name: 'home',
@@ -49,6 +113,9 @@ export default {
 
 <style lang="sass">
   @import "@/variables.sass"
+
+  .home
+    margin-top: 64px
 
   #nav
     background: $orange
@@ -106,4 +173,12 @@ export default {
 
         &.best
           transform: translate3d(128px, 0, 0)
+
+  ul.stories
+    list-style: none
+    margin: 0
+
+    li
+      border-bottom: 1px solid #D0D0D0
+      list-style: none
 </style>
